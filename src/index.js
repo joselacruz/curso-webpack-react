cff1513f75ee511ee78445f190d27fb81ebb4d38
joselacruz <joselacruzperez@gmail.com>
2023-03-10 16:11:32 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import ReactDom from 'react-dom';
+import App from './components/App';
+import './styles/global.scss';
+
+ReactDom.render( <App/>, document.getElementById('app'));
+
